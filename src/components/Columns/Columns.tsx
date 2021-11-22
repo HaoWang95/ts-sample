da35176d42db6f5e36254dd40436d9375fef8f3c
HaoWang95 <alanwang0028@gmail.com>
2021-11-23 00:13:50 +1100

    drag and drop complete

--- src/components/Columns/Columns.tsx
+++ src/components/Columns/Columns.tsx
@@ -2,7 +2,10 @@ import { ColumnContainer, ColumnTitle } from "../../style";
 import { AddNewItem } from "../AddNewItem";
 import { useAppGlobalState } from "../../state/AppStateContext";
 import { Card } from "../Card";
-import { addTask } from "../../state/action";
+import { addTask, moveList } from "../../state/action";
+import { useRef } from "react";
+import { useItemDrag } from "../../utils/useItemDrag";
+import { useDrop } from "react-dnd";
 
 // type React.PropsWithChildren<P> = P & {children ?: React.ReactNode}
 
@@ -10,7 +13,6 @@ type ColumnProps = {
     text: string;
     id: string;
 }
-
 // Another implementation
 // type ColProps = {
 //     text: string;
@@ -18,12 +20,27 @@ type ColumnProps = {
 // }
 
 export const Column = ({ text, id }: ColumnProps) => {
-  const { getTaskByListId, dispatch } = useAppGlobalState();
+  const { draggedItem, getTaskByListId, dispatch } = useAppGlobalState();
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({type:"COLUMN", id, text});
+
+  const [, drop] = useDrop({
+    accept:"COLUMN",
+    hover(){
+      if(!draggedItem) return;
+      if(draggedItem.type === "COLUMN"){
+        if(draggedItem.id === id) return;
+      }
+      dispatch(moveList(draggedItem.id, id));
+    }
+  })
+
+  drag(drop(ref));
 
   const tasks = getTaskByListId(id); 
   console.log(`In column ${tasks}`);
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text ? text : "Column Title"}</ColumnTitle>
       {tasks.map(task => (
           <Card id={task.id} text={task.text} key={task.id} />
